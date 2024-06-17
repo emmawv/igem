985a432e3a408cfd91e076dfe495f768ceaa8604
Emma Willett <emmawv@gmail.com>
2024-06-17 18:43:42 +0200

    changes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TEAM_ID: string = 'Madrid-UCM';
-  readonly VITE_TEAM_NAME: string = 'Madrid-UCM';
-  readonly VITE_TEAM_YEAR: string = '2024';
+  readonly VITE_TEAM_ID: string;
+  readonly VITE_TEAM_NAME: string;
+  readonly VITE_TEAM_YEAR: string;
 }
 
-export interface ImportMeta {
+interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
